d246dc92c36edf658034d86019f7329635fd0431
Syancheg <syancheg@gmail.com>
2021-04-23 17:32:08 +0300

    HomeWork7

---
+++ PingPong/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PingPong
+//
+//  Created by Константин Кузнецов on 23.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
